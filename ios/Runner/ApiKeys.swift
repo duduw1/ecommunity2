e9b2e48926317a9e1e404803c49bfa559cbee1ad
duduw1 <8du@hotmail.com.br>
2026-01-09 01:23:39 -0300

    ocultando chaves

---
+++ ios/Runner/ApiKeys.swift
@@ -0,0 +1,3 @@
+struct ApiKeys {
+    static let googleMapsApiKey = "AIzaSyAZozHCnGdzmSkgFA5X1DfC5_qGPIx5nvg"
+}
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyAZozHCnGdzmSkgFA5X1DfC5_qGPIx5nvg")
+    GMSServices.provideAPIKey(ApiKeys.googleMapsApiKey)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
